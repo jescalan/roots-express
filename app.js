6d01667a658542b095bbadb4e1c68c7cd3296e55
Jeff Escalante <hello@jenius.me>
2012-11-17 14:59:55 -0500

    looking great, livereload and roots css working perfectly

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(require('stylus').middleware({ src: __dirname + '/public', compile: roots.middleware }));
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
